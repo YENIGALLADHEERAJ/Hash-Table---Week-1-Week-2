8d45045f5c94a0896b442a3e795428fee5003a66
YENIGALLADHEERAJ <dheerajyenigalla@gmail.com>
2026-04-22 15:51:12 +0530

    UC1: Add HashMap insertion and display

--- src/HashTableApp.java
+++ src/HashTableApp.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+
 public class HashTableApp {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> students = new HashMap<>();
+
+        students.put(101, "Dheeraj");
+        students.put(102, "Krishna");
+        students.put(103, "Shritan");
+
+        System.out.println("Student Records:");
+        System.out.println(students);
+    }
 }
