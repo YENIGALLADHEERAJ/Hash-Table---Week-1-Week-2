b277350a820126d6a0a431d87c611722d53a0ef0
YENIGALLADHEERAJ <dheerajyenigalla@gmail.com>
2026-04-22 16:00:31 +0530

    UC8: Add username availability checker

--- src/HashTableApp.java
+++ src/HashTableApp.java
@@ -3,23 +3,18 @@ import java.util.HashMap;
 public class HashTableApp {
     public static void main(String[] args) {
 
-        int[] numbers = {2, 7, 11, 15};
-        int target = 9;
+        HashMap<String, Boolean> users = new HashMap<>();
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        users.put("dheeraj", true);
+        users.put("krishna", true);
+        users.put("shritan", true);
 
-        for (int i = 0; i < numbers.length; i++) {
-            int complement = target - numbers[i];
+        String newUsername = "krishna";
 
-            if (map.containsKey(complement)) {
-                System.out.println("Pair Found:");
-                System.out.println(complement + " + " + numbers[i] + " = " + target);
-                return;
-            }
-
-            map.put(numbers[i], i);
+        if (users.containsKey(newUsername)) {
+            System.out.println("Username already taken");
+        } else {
+            System.out.println("Username available");
         }
-
-        System.out.println("No Pair Found");
     }
 }
